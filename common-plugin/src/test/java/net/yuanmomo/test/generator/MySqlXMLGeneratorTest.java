7fd670f611196347d58aa7b32dcbaa0ecac26e08
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2017-06-10 01:33:40 +0800

    update read me

---
+++ common-plugin/src/test/java/net/yuanmomo/test/generator/MySqlXMLGeneratorTest.java
@@ -0,0 +1,26 @@
+package net.yuanmomo.test.generator;
+
+
+import net.yuanmomo.mybatis.mbg.table.MySqlTableXmlPrinter;
+import org.junit.Test;
+
+import java.util.List;
+
+public class MySqlXMLGeneratorTest {
+    @Test
+	public void generate(){
+		/**
+		 * default host : 127.0.0.1
+		 * defult port: 3306
+		 */
+		MySqlTableXmlPrinter mySqlTableXmlPrinter1 = new MySqlTableXmlPrinter("root","root");
+
+//		MySqlTableXmlPrinter mySqlTableXmlPrinter2 = new MySqlTableXmlPrinter("host","port","username","password");
+
+		// three databases named : test, demo, todo
+		List<String> output = mySqlTableXmlPrinter1.print("test","demo","todo");
+		for(String str : output){
+			System.out.println(str);
+		}
+	}
+}
