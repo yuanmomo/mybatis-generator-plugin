bbc03f394af359084dd02d5f40f1c7ba8fd41478
aaaaa <aaaa@gmail.com>
2019-03-08 10:31:52 +0800

    add selectOne with blob

--- common-mbg-plugin/src/main/java/com/github/yuanmomo/mybatis/mbg/plugin/selectone/ProviderSelectOneByExampleWithoutBLOBsMethodGenerator.java
+++ common-mbg-plugin/src/main/java/com/github/yuanmomo/mybatis/mbg/plugin/selectone/ProviderSelectOneByExampleWithoutBLOBsMethodGenerator.java
@@ -1,5 +1,12 @@
 package com.github.yuanmomo.mybatis.mbg.plugin.selectone;
 
+import static org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities.getSelectListPhrase;
+import static org.mybatis.generator.internal.util.StringUtility.escapeStringForJava;
+
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.api.dom.java.JavaVisibility;
@@ -7,13 +14,6 @@ import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.api.dom.java.Parameter;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
 import org.mybatis.generator.codegen.mybatis3.javamapper.elements.sqlprovider.AbstractJavaProviderMethodGenerator;
-
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-
-import static org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities.getSelectListPhrase;
-import static org.mybatis.generator.internal.util.StringUtility.escapeStringForJava;
 
 public class ProviderSelectOneByExampleWithoutBLOBsMethodGenerator extends AbstractJavaProviderMethodGenerator {
 
@@ -83,7 +83,7 @@ public class ProviderSelectOneByExampleWithoutBLOBsMethodGenerator extends Abstr
     }
 
     public List<IntrospectedColumn> getColumns() {
-        return introspectedTable.getNonBLOBColumns();
+        return introspectedTable.getAllColumns();
     }
 
 
